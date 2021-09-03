ccbead6e222230586e67606930a4db0ac3ee9de4
Max <max.omeleneckii@gmail.com>
2021-09-03 18:22:16 +0300

    init: add js cod

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.first;
+        this.last;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last = max;
     }
-
     guess() {
-
+        return (this.middle = Math.round((this.last + this.first) / 2));
     }
-
     lower() {
-
+        this.last = this.middle;
     }
-
     greater() {
-
+        this.first = this.middle;
     }
 }
 
